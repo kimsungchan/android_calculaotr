d1515dc318422a1ecc8dd75cb8645272a58162a8
서영균 <vault961@gmail.com>
2016-11-18 17:56:47 +0900

    project 생성 및 계산기 더하기 빼기 곱하기 나누기 기능 구현

---
+++ app/src/main/java/com/example/a403/myapplication/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.a403.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText n1;
+    EditText n2;
+    TextView t1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        n1 = (EditText)findViewById(R.id.editText);
+        n2 = (EditText)findViewById(R.id.editText2);
+        t1 = (TextView)findViewById(R.id.textView);
+    }
+       void on(View v){
+           int num1 = Integer.parseInt((n1.getText().toString()));
+           int num2 = Integer.parseInt((n2.getText().toString()));
+           int result = 0;
+
+           if(v.getId() == R.id.button){
+               result = num1+num2;
+           }
+           else if(v.getId() == R.id.button2){
+               result = num1-num2;
+           }
+           else if(v.getId() == R.id.button3){
+               result = num1*num2;
+           }
+           else if(v.getId() == R.id.button4){
+               result = num1/num2;
+           }
+           t1.setText(""+result);
+    }
+}
